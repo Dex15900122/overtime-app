aa994bf2ee53380661fc56f1422312efa58bc709
Dex15900122 <e5900122@yahoo.com.tw>
2018-03-02 20:41:43 +0800

    Implemented edit functionality for status

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,7 +47,7 @@ class PostsController < ApplicationController
   private
 
     def post_params
-      params.require(:post).permit(:date, :rationable)
+      params.require(:post).permit(:date, :rationable, :status)
     end
 
 
