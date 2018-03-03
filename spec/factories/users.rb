d7ea239b32d65a793a0dbf01f09098a9c8ae920a
Dex15900122 <e5900122@yahoo.com.tw>
2018-03-03 17:54:47 +0800

    Implemented tests and page redirection for permission structure

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -20,5 +20,13 @@ FactoryGirl.define do
     password_confirmation "123456"
   end
 
+  factory :non_authorized_user, class: "User" do
+    first_name 'Non'
+    last_name 'Authorized'
+    email { generate :email }
+    password "asdfasdf"
+    password_confirmation "asdfasdf"
+  end
+
 end
 
