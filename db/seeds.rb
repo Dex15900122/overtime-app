576379a08ca18a7de83504199237b05e066d8692
Dex15900122 <e5900122@yahoo.com.tw>
2018-03-05 13:31:13 +0800

    Integrated phone validations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
-@user = User.create(email: "123456@xxx.com", password: "123456", password_confirmation: "123456", first_name: "Dex", last_name: "Kuo")
+@user = User.create(email: "123456@xxx.com", 
+                    password: "123456", 
+                    password_confirmation: "123456", 
+                    first_name: "Dex", 
+                    last_name: "Kuo",
+                    phone: "2252404065 ")
 
 puts "1 User created"
 
-AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Name")
+AdminUser.create(email: "admin@test.com", 
+                 password: "asdfasdf", 
+                 password_confirmation: "asdfasdf", 
+                 first_name: "Admin", 
+                 last_name: "Name",
+                 phone: "2252404065 ")
 
 puts "1 Admin User created"
 
